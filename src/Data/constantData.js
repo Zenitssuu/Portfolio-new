66e9caecc287f055486124e6976e5548237b4df5
Siddhant <154405878+Zenitssuu@users.noreply.github.com>
2025-06-09 23:19:00 +0530

    Update resume in constantData.js

--- src/Data/constantData.js
+++ src/Data/constantData.js
@@ -3,5 +3,5 @@ export const myData = {
     github: "https://github.com/zenitssuu/",
     // insta: "https://www.instagram.com/iam_mongalmoy/",
     linkedin: "https://linkedin.com/in/siddhant-saiba-9a46231aa/",
-    resumeLink: "https://drive.google.com/file/d/1DreRm7LhPyjy9NAYlBGKC4e2T3NCb1pR/view?usp=drive_link",
+    resumeLink: "https://drive.google.com/file/d/1f2WQY9JpErCJxmxrDIhhOg-In_zTMS-5/view?usp=sharing",
   }
